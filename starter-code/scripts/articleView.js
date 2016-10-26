8f3eb6fcd5955b6422cb415ed842018b96b7fc8f
Daniel Becker <dbecker4130@gmail.com>
2016-10-26 16:34:50 -0700

    half to-dos complete

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -19,13 +19,18 @@ articleView.populateFilters = function() {
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     if ($(this).val()) {
+      $('article').hide();
+      $('article[data-author="' + $(this).val() + '"]').fadeIn();
       /* TODO: If the slect box changes to an option that has a value, we should:
           1. Hide all of the articles
           2. Fade in only the articles that match based on on the author
             that was aselected. Hint: use an attribute selector to find
             those articles that match the value, and then fade them in.
         */
+        // done
     } else {
+      $('article').fadeIn();
+      $('article.template').hide();
     /* Otherwise, we should:
         1. Show all the articles except the template */
     }
@@ -37,7 +42,19 @@ articleView.handleCategoryFilter = function() {
   /* TODO: Just like we do for #author-filter above, we should also handle
   change events on the #category-filter element. Be sure to reset the
   #author-filter while you're at it! */
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $('article[data-category="' + $(this).val() + '"]').fadeIn();
+
+    } else {
+      $('article').fadeIn();
+      $('article.template').hide();
+    }
+    $('#author-filter').val('');
+  });
 };
+
 
 articleView.handleMainNav = function () {
   $('.main-nav').on('click', '.tab', function() {
@@ -46,6 +63,8 @@ articleView.handleMainNav = function () {
       2. Fade in the single .tab-content section that is
         associated with the .tab element's data-content attribute.
     */
+    // $('.tab-content').hide();
+
   });
   $('.main-nav .tab:first').click();
 };
@@ -56,7 +75,7 @@ articleView.setTeasers = function() {
   /* TODO: Add a delegated event handler to reveal the remaining paragraphs.
     When a .read-on link is clicked, we can:
     1. Prevent the default action of a link.
-    2. Reveal everything in that particular article now.
+    2. Reveal everything in that particular article now. (.this value)
     3. Hide that read-on link!
 
     // STRETCH GOAl!: change the 'Read On' link to 'Show Less'
@@ -64,3 +83,10 @@ articleView.setTeasers = function() {
 };
 
 // TODO: Invoke all of the above functions (I mean, methods!):
+// done
+
+articleView.populateFilters();
+articleView.handleAuthorFilter();
+articleView.handleCategoryFilter();
+articleView.handleMainNav();
+articleView.setTeasers();
